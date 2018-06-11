740a8aa0450aa94097d8bfe3c428919bb8447467
Matthew-Kanderka <kanderkam@hotmail.com>
2018-06-10 22:02:18 -0600

    Added more info for results output

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ app.get("/", function(req, res) {
 });
 
 app.get("/results", function(req, res){
-	request("http://www.omdbapi.com/?apikey=thewdb&s=calgary", function(error, response, body){
+	var query = (req.query.search);
+	var url = "http://www.omdbapi.com/?apikey=thewdb&s=" + query;
+	request(url, function(error, response, body){
 		if(!error && response.statusCode == 200) {
 			var data = JSON.parse(body);
 			res.render("results", {data : data});
